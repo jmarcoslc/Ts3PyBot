1f5ab8e80c302009a5db71dea2dceca3f9f03ab5
jmarcoslc <jmarcoslc@gmail.com>
2016-08-21 13:43:09 +0200

    Update bot_update.sh

--- bot_update.sh
+++ bot_update.sh
@@ -9,9 +9,9 @@ unzip master.zip;
 downloaded_version=`cat $PY_NAME-master/$PY_NAME.py | grep "INSTALLED_VERSION = " | cut -d\" -f2`;
 
 if [ $1 -ne $downloaded_version ]; then
-	service ts3bot.sh stop							# ts3bot.sh is not provided, as serveradmin you must to deploy your own script
+	service ts3bot.sh stop				# ts3bot.sh is not provided, as serveradmin you must to deploy your own script
 	cp $PY_NAME-master/$PY_NAME.py ./$PY_NAME.py 	# take these three lines as an example
-	service ts3bot.sh start -f 						# 
+	service ts3bot.sh start -f 			# 
 else
 	rm -R $PY_NAME-master;
 	rm master.zip;
