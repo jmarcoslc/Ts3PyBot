831434292eadabbd610247c5970c3f2c03012908
jmarcoslc <jmarcoslc@gmail.com>
2016-08-21 15:13:54 +0200

    Update Ts3PyBot.py

--- Ts3PyBot.py
+++ Ts3PyBot.py
@@ -513,7 +513,7 @@ def channel_message_handler(user, message, userid, uniqueid):
 def command_botupdate(text_after_command, userid):
 	if check_if_superadmin(userid):
 		send_text_to_channel(my_channel, "Actualizando BOT")
-		subprocess.call(["bot_update.sh", str(INSTALLED_VERSION)])
+		subprocess.call(["./bot_update.sh", str(INSTALLED_VERSION)])
 		send_text_to_channel(my_channel, "No hay actualización disponible") #If reach this line, there is no update available.
 
 def command_kick(user, text_after_command, userid, uniqueid):
