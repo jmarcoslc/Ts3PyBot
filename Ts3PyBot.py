8ae0c3c2fd0d45030ba7e00ce6f3922590db3a66
Aens <saelyth@hotmail.com>
2016-08-21 22:10:09 +0200

    Update Ts3PyBot.py

--- Ts3PyBot.py
+++ Ts3PyBot.py
@@ -35,7 +35,7 @@ from urllib import request
 from random import choice, sample
 from login_data_file import *
 
-INSTALLED_VERSION = "5.3"
+INSTALLED_VERSION = "5.2.1"
 VERSION_COMMENTS = "Comando de actualización del bot mejorado."
 
 #################
