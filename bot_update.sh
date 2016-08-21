8794e3049beb1e7d7a2a2ee6392b84dd0f480859
jmarcoslc <jmarcoslc@gmail.com>
2016-08-21 15:43:14 +0200

    Update bot_update.sh

--- bot_update.sh
+++ bot_update.sh
@@ -2,17 +2,19 @@
 
 URL_MASTER="https://github.com/Aens/Ts3PyBot/archive/master.zip";
 PY_NAME="Ts3PyBot";
+SHELL_SCRIPT_NAME="bot_update";
 
 wget $URL_MASTER;
 unzip master.zip;
 
 downloaded_version=`cat $PY_NAME-master/$PY_NAME.py | grep "INSTALLED_VERSION = " | cut -d\" -f2`;
 
-if [ $1 -ne $downloaded_version ]; then
-	service ts3bot.sh stop				# ts3bot.sh is not provided, as serveradmin you must to deploy your own script
-	cp $PY_NAME-master/$PY_NAME.py ./$PY_NAME.py 	# take these three lines as an example
-	service ts3bot.sh start -f 			# 
-else
-	rm -R $PY_NAME-master;
-	rm master.zip;
+if [ "$1" != "$downloaded_version" ]; then
+	service ts3bot.sh stop												# ts3bot.sh is not provided, as serveradmin you must to deploy your own script
+	cp $PY_NAME"-master"/$PY_NAME.py ./$PY_NAME.py 						# take these lines as an example
+	cp $PY_NAME"-master"/$SHELL_SCRIPT_NAME.sh ./$SHELL_SCRIPT_NAME.sh
+	service ts3bot.sh start -f
 fi
+
+rm -R $PY_NAME"-master";
+rm master.zip;
